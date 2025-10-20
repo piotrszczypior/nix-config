6dbc0fc0bf991e3c0580f4667047254aa905cef6
Piotr Szczypior <piotrszczypior.mail@gmail.com>
2025-10-21 00:12:11 +0200

    move comment from configuration.nix

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -5,4 +5,10 @@
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 }
